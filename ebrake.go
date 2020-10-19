fc01312b0bd0281a1d9c43e6232858cf12f7f380
John Shimek <jshimek@backblaze.com>
2020-10-19 10:49:00 -0500

    Initial commit.

---
+++ ebrake.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+type Encoder struct {
+	config *Config
+}
+
+type Video struct {
+	source string
+	target string
+}
+
+// EncodeFiles encodes all the video files in the source directory and places them
+// in the destination directory.
+func (encoder *Encoder) EncodeFiles() error {
+
+	info, err := os.Stat(encoder.config.Target)
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		return errors.New(encoder.config.Target + " is not a directory")
+	}
+
+	videoFiles, err := encoder.getVideoFiles()
+
+	if err != nil {
+		return err
+	}
+
+	var videos []Video
+	for _, videoFile := range videoFiles {
+		targetFile, err := encoder.getDestinationPath(videoFile)
+		if err != nil {
+			return err
+		}
+		videos = append(videos, Video{
+			source: videoFile,
+			target: targetFile,
+		})
+	}
+	for _, video := range videos {
+		fmt.Println("handbrake.exe ", video.source, video.target)
+	}
+
+	return nil
+}
+
+// getVideoFiles finds and returns the list of video files in the given directory.
+// The test for video file is based on the file extension.
+func (encoder *Encoder) getVideoFiles() ([]string, error) {
+	var videoFiles []string
+	err := filepath.Walk(encoder.config.Source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			// TODO log error and return SkipDir?
+			return errors.Wrap(err, "failed to walk source directory")
+		}
+		if encoder.isVideoFile(path, info) {
+			videoFiles = append(videoFiles, path)
+		}
+		return nil
+	})
+	return videoFiles, err
+}
+
+// isVideoFile returns whether the given path is a video file based on the extension.
+func (encoder *Encoder) isVideoFile(path string, info os.FileInfo) bool {
+	if info.IsDir() {
+		return false
+	}
+
+	ext := filepath.Ext(path)
+	for _, ve := range encoder.config.SourceExtensions  {
+		if ve == ext {
+			return true
+		}
+	}
+	return false
+}
+
+// getDestinationPath returns the path to the new file based at the destination.
+func (encoder *Encoder) getDestinationPath(videoPath string) (string, error) {
+	rel, err := filepath.Rel(encoder.config.Source, videoPath)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to find relative path to source file")
+	}
+	destinationPath := filepath.Join(encoder.config.Target, rel)
+	ext := filepath.Ext(destinationPath)
+	destinationPath = strings.TrimSuffix(destinationPath, ext)
+	destinationPath = destinationPath + encoder.config.TargetExtension
+	return destinationPath, nil
+}
+
---
+++ main.go
@@ -0,0 +1,113 @@
+/*
+MIT License
+
+Copyright (c) 2020 John Shimek
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package main
+
+import (
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+)
+
+type Config struct {
+	Source           string
+	Target           string
+	SourceExtensions []string
+	TargetExtension  string
+}
+
+var cfgFile string
+
+// init initialized cobra for the CLI flags
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile,
+		"config",
+		"",
+		"config file (default is $HOME/.ebrake.yaml)",
+	)
+}
+
+// main let's go dude
+func main() {
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	viper.SetDefault("SourceExtensions", []string{".mp4", ".mkv"})
+	viper.SetDefault("TargetExtension", ".mp4")
+
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".ebrake" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".ebrake")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+// rootCmd represents the base CLI command
+var rootCmd = &cobra.Command{
+	Use:   "ebrake",
+	Short: "Re-encodes a directory of movie files using Handbrake.",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		config := Config{
+			Source:      args[0],
+			Target: args[1],
+		}
+		if err := viper.Unmarshal(&config); err != nil {
+			fmt.Println("Error: " + err.Error())
+			os.Exit(1)
+		}
+
+		encoder := Encoder{config: &config}
+		if err := encoder.EncodeFiles(); err != nil {
+			fmt.Println("Error: " + err.Error())
+			os.Exit(1)
+		}
+	},
+}
